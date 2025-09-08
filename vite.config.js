599dd25e2293cc089dc23ca9e386afcf62297689
Vedant Chetanbhai Saparia <vedantchetanbhaisaparia@Vedants-MacBook-Air.local>
2025-09-08 22:29:37 +0530

    Minor Changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Static_Admin_Dashboard_React',
+  base:'/Static_Admin_Dashboard_React/',
   server:{
     port:3000,
   },
